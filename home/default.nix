ac9c530bfa6f03feec904aa7e0972189170d93db
Laevatein Scarlet <fish_magic@ftmc.me>
2023-12-25 23:58:36 +0800

    add more java

--- home/default.nix
+++ home/default.nix
@@ -7,11 +7,23 @@
 
   home.stateVersion = "23.11";
 
-  home.packages = with pkgs; [
+  home.packages = with pkgs; let
+    temurin-bin-8-priority-8 = temurin-bin-8.overrideAttrs(oldAttrs: {
+      meta.priority = 8;
+    });
+    temurin-bin-11-priority-11 = temurin-bin-11.overrideAttrs(oldAttrs: {
+      meta.priority = 11;
+    });
+    temurin-bin-17-priority-17 = temurin-bin-8.overrideAttrs(oldAttrs: {
+      meta.priority = 17;
+    });
+  in [
     hyfetch
     python310
     libgcc
     automake
-    temurin-bin-17
+    temurin-bin-8-priority-8
+    temurin-bin-11-priority-11
+    temurin-bin-17-priority-17
   ];
 }
--- home/programs/fish.nix
+++ home/programs/fish.nix
@@ -1,3 +1,11 @@
+{ config, lib, pkgs, ... }:
+
 {
   home.file.".config/fish/functions/fish_greeting.fish".source = ./fish_greeting.fish;
+
+  programs.fish.shellAliases = {
+      "java8" = "${pkgs.temurin-bin-8}/bin/java";
+      "java11" = "${pkgs.temurin-bin-11}/bin/java";
+      "java17" = "${pkgs.temurin-bin-17}/bin/java";
+  };
 }
