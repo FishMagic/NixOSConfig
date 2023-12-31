4da2ff06cdc98081ed699caa537fe1768cbf7da2
Laevatein Scarlet <fish_magic@ftmc.me>
2023-12-31 19:21:45 +0800

    remove pkgs javas

--- home/default.nix
+++ home/default.nix
@@ -7,23 +7,11 @@
 
   home.stateVersion = "23.11";
 
-  home.packages = with pkgs; let
-    temurin-bin-8-priority-8 = temurin-bin-8.overrideAttrs(oldAttrs: {
-      meta.priority = 8;
-    });
-    temurin-bin-11-priority-11 = temurin-bin-11.overrideAttrs(oldAttrs: {
-      meta.priority = 11;
-    });
-    temurin-bin-17-priority-17 = temurin-bin-8.overrideAttrs(oldAttrs: {
-      meta.priority = 17;
-    });
-  in [
+  home.packages = with pkgs;
+  [
     hyfetch
     python310
     libgcc
     automake
-    temurin-bin-8-priority-8
-    temurin-bin-11-priority-11
-    temurin-bin-17-priority-17
   ];
 }
